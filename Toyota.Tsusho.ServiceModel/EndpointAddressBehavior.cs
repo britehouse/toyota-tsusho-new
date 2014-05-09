8879c7b540f930ffab2af45c7add2a40371686e7
pieterv-icloud-com <pieterv@icloud.com>
2014-05-09 14:05:36 +0200

    Added a ServiceModel Project

---
+++ Toyota.Tsusho.ServiceModel/EndpointAddressBehavior.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toyota.Tsusho.ServiceModel
+{
+    public class EndpointAddressBehavior : Attribute, IWsdlExportExtension, IEndpointBehavior, IServiceBehavior
+    {
+        private readonly EndpointAddressBehaviorExtensionElement configuration;
+
+        public EndpointAddressBehavior(EndpointAddressBehaviorExtensionElement configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint,
+            BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint,
+            ClientRuntime clientRuntime)
+        {
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint,
+            EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+
+        public void AddBindingParameters(ServiceDescription serviceDescription,
+            ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints,
+            BindingParameterCollection bindingParameters)
+        {
+            // If we are added as service behavior, add us to all the endpoints
+            foreach (ServiceEndpoint endpoint in endpoints)
+            {
+                if (!endpoint.Behaviors.Contains(typeof(EndpointAddressBehavior)))
+                    endpoint.Behaviors.Add(this);
+            }
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription,
+            ServiceHostBase serviceHostBase)
+        {
+        }
+
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+        }
+
+        public void ExportContract(WsdlExporter exporter,
+            WsdlContractConversionContext context)
+        {
+        }
+
+        public void ExportEndpoint(WsdlExporter exporter, WsdlEndpointConversionContext context)
+        {
+            UriBuilder builder = new UriBuilder(this.configuration.Address);
+
+            builder.Scheme = context.Endpoint.Binding.Scheme;
+
+            switch (this.configuration.HostReplace)
+            {
+                case HostReplace.MachineName:
+                    {
+                        builder.Host = Environment.MachineName  ;
+                        break;
+                    }
+
+                case HostReplace.FQDN:
+                    {
+                        builder.Host = string.Format("{0}.{1}", Environment.MachineName, IPGlobalProperties.GetIPGlobalProperties().DomainName);
+                        break;
+                    }
+
+                case HostReplace.IPAddress:
+                    {
+                        builder.Host = GetIPAddress();
+                        break;
+                    }
+            }
+
+            context.Endpoint.Address = new EndpointAddress(builder.Uri.ToString());
+        }
+
+        private string GetIPAddress()
+        {
+            return Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+        }
+    }
+}
---
+++ Toyota.Tsusho.ServiceModel/EndpointAddressBehaviorExtensionElement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toyota.Tsusho.ServiceModel
+{
+    public enum HostReplace
+    {
+        None,
+        MachineName,
+        FQDN,
+        IPAddress
+    }
+
+    public class EndpointAddressBehaviorExtensionElement : BehaviorExtensionElement
+    {
+        [ConfigurationProperty("address", IsRequired=true)]
+        public string Address
+        {
+            get { return (string) base["address"]; }
+            set { base["address"] = value; }
+        }
+
+        [ConfigurationProperty("hostReplace", DefaultValue= HostReplace.None)]
+        public HostReplace HostReplace
+        {
+            get { return (HostReplace)base["hostReplace"]; }
+            set { base["hostReplace"] = value; }
+        }
+
+        public override Type BehaviorType
+        {
+            get
+            {
+                return typeof(EndpointAddressBehavior);
+            }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new EndpointAddressBehavior(this);
+        }
+    }
+}
