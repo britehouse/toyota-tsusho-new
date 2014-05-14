2ddaf37960688631d49d50944b81e05131b28767
pieterv-icloud-com <pieterv@icloud.com>
2014-05-14 12:17:46 +0200

    Number 5

---
+++ Toyota.Tsusho.TALO.API.DataContracts/Vehicle/RegistrationDataContract.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Toyota.Tsusho.TALO.API.DataContracts.Vehicle
+{
+    /// <summary>
+    /// Describes a Vehicle Registration.
+    /// </summary>
+    [DataContract(Namespace = "http://www.ttaf.co.za/talo/api/2014/05/vehicle")]
+    public class RegistrationDataContract
+    {
+        [DataMember(Order = 0, IsRequired = true)]
+        public string VehicleNumber
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Order = 1, IsRequired = true)]
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Order = 3, IsRequired = true)]
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Order = 4, IsRequired = true)]
+        public string Number
+        {
+            get;
+            set;
+        }
+    }
+}
