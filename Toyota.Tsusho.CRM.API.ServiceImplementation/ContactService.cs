b9513b4a6bc8aba4113c9028e6093e6303c527f0
pieterv-icloud-com <pieterv@icloud.com>
2014-08-20 12:06:57 +0200

    Updated CRM Integration Solution

---
+++ Toyota.Tsusho.CRM.API.ServiceImplementation/ContactService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toyota.Tsusho.CRM.API.ServiceImplementation
+{
+    [ServiceBehavior(Name = "ContactService",
+        Namespace = "http://www.ttaf.co.za/crm/api/2014/08",
+        InstanceContextMode = InstanceContextMode.PerSession,
+        ConcurrencyMode = ConcurrencyMode.Single)]
+    public abstract class ContactServiceBase : Toyota.Tsusho.CRM.API.ServiceContracts.IContactService
+    {
+        public void Publish(Toyota.Tsusho.CRM.API.MessageContracts.ContactPublishRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class ContactService : ContactServiceBase
+    {
+
+    }
+}
