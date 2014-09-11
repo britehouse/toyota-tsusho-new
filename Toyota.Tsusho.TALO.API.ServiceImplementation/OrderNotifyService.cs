7113292a3e0d39fca92fd554197810b990b78166
pieterv-icloud-com <pieterv@icloud.com>
2014-09-11 13:31:07 +0200

    Fixed Bindings

--- Toyota.Tsusho.TALO.API.ServiceImplementation/OrderNotifyService.cs
+++ Toyota.Tsusho.TALO.API.ServiceImplementation/OrderNotifyService.cs
@@ -13,11 +13,6 @@ namespace Toyota.Tsusho.TALO.API.ServiceImplementation
         {
             //TODO: Implement logic here to update TALO order information.
         }
-
-        public void ManualCheckNotify(MessageContracts.ManualCheckNotifyRequestMessage request)
-        {
-            //TODO: Implement logic here to update TALO order information.
-        }
     }
 
     public partial class OrderNotifyService : OrderNotifyServiceBase
