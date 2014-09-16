adf54e1d9388ddb3b829069d77912fba3390e364
pieterv-icloud-com <pieterv@icloud.com>
2014-09-16 14:54:37 +0200

    Added a Command Line option

--- CrmSvcUtilsExtensions/GenerateWCFCustomizations.bat
+++ CrmSvcUtilsExtensions/GenerateWCFCustomizations.bat
@@ -2,4 +2,5 @@ CrmSvcUtil.exe ^
 /codecustomization:"CrmSvcUtilExtensions.WCFCustomizationService, CrmSvcUtilExtensions" ^
 /url:http://crm2013:5555/CRM/XRMServices/2011/Organization.svc ^
 /out:CRMTypes.cs ^
-/namespace:Toyota.Tsusho.CRM.API
+/namespace:Toyota.Tsusho.CRM.API ^
+/serviceConttextName:ServiceContext
