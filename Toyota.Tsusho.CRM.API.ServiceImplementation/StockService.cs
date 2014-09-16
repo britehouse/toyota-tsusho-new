5912f49ca023f2cf4f5f56ddb65a697e9bc7b726
pieterv-icloud-com <pieterv@icloud.com>
2014-09-16 15:58:54 +0200

    Finished Coding No 4

--- Toyota.Tsusho.CRM.API.ServiceImplementation/StockService.cs
+++ Toyota.Tsusho.CRM.API.ServiceImplementation/StockService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk.Client;
+using Toyota.Tsusho.CRM.API.DataContracts;
 
 
 namespace Toyota.Tsusho.CRM.API.ServiceImplementation
@@ -22,6 +23,37 @@ namespace Toyota.Tsusho.CRM.API.ServiceImplementation
         {
             using (OrganizationServiceProxy proxy = CRMHelper.Connect())
             {
+                ServiceContext context = new ServiceContext(proxy);
+
+                foreach(StockItemDataContract item in request.Items)
+                {
+                    bool add = false;
+
+                    new_stock record = (from a in context.new_stockSet
+                                           where a.new_vinnumber == item.Stock.new_vinnumber
+                                           select a).FirstOrDefault();
+
+                    if(record == null)
+                    {
+                        add = true;
+
+                        record = new new_stock();
+                    }
+
+                    record.new_internalvehiclenumber = item.Stock.new_internalvehiclenumber;
+                    record.new_vinnumber = item.Stock.new_vinnumber;
+                    record.new_enginenumber = item.Stock.new_enginenumber;
+                    record.new_modelcode = item.Stock.new_modelcode;
+                    record.new_plant = item.Stock.new_plant;
+                    record.new_storagelocation = item.Stock.new_storagelocation;
+                    record.new_stockvalue = item.Stock.new_stockvalue;
+                    record.new_status = item.Stock.new_status;
+
+                    if(add)
+                        proxy.Create(record);
+                    else
+                        proxy.Update(record);
+                }
             }
         }
     }
