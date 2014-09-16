c6fe5778e6cef949435659d8c9d8ab3b6dfca917
pieterv-icloud-com <pieterv@icloud.com>
2014-09-16 15:29:53 +0200

    Added a ServiceContext

--- CrmSvcUtilsExtensions/GenerateWCFCustomizations.bat
+++ CrmSvcUtilsExtensions/GenerateWCFCustomizations.bat
@@ -1,6 +1,6 @@
 CrmSvcUtil.exe ^
 /codecustomization:"CrmSvcUtilExtensions.WCFCustomizationService, CrmSvcUtilExtensions" ^
 /url:http://crm2013:5555/CRM/XRMServices/2011/Organization.svc ^
-/out:CRMTypes.cs ^
+/out:Types.cs ^
 /namespace:Toyota.Tsusho.CRM.API ^
-/serviceConttextName:ServiceContext
+/serviceContextName:ServiceContext
