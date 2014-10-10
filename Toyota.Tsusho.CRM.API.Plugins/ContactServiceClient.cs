ed9fd0f90ee4280dee8bf8bd2987712fef19f8ae
pieterv-icloud-com <pieterv@icloud.com>
2014-10-10 08:26:35 +0200

    Zombie Projects

---
+++ Toyota.Tsusho.CRM.API.Plugins/ContactServiceClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toyota.Tsusho.CRM.API.Plugins
+{
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    public partial class ContactServiceClient : System.ServiceModel.ClientBase<Toyota.Tsusho.CRM.API.Plugins.IContactService>, Toyota.Tsusho.CRM.API.Plugins.IContactService
+    {
+
+        public ContactServiceClient()
+        {
+        }
+
+        public ContactServiceClient(string endpointConfigurationName) :
+            base(endpointConfigurationName)
+        {
+        }
+
+        public ContactServiceClient(string endpointConfigurationName, string remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ContactServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public ContactServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+            base(binding, remoteAddress)
+        {
+        }
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        public void Publish(Toyota.Tsusho.CRM.API.MessageContracts.ContactPublishRequestMessage request)
+        {
+            base.Channel.Publish(request);
+        }
+
+    }
+}
---
+++ Toyota.Tsusho.CRM.API.Plugins/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toyota.Tsusho.CRM.API.Plugins
+{
+    [System.ServiceModel.ServiceContractAttribute(Namespace = "http://www.ttaf.co.za/crm/api/2014/08", ConfigurationName = "Toyota.Tsusho.CRM.Services.Generic.OnRamp")]
+    public interface IContactService
+    {
+        [System.ServiceModel.OperationContractAttribute(Action = "http://www.ttaf.co.za/crm/api/2014/08/IContactService/Publish")]
+        void Publish(Toyota.Tsusho.CRM.API.MessageContracts.ContactPublishRequestMessage request);
+    }
+}
