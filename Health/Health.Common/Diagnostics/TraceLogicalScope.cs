8cb65541c58d51126e30aab486467e1bb75997cc
pieterv-icloud-com <pieterv@icloud.com>
2015-01-18 19:18:45 +0200

    Added Diagnostics

---
+++ Health/Health.Common/Diagnostics/TraceLogicalScope.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Health.Common.Diagnostics
+{
+    /// <summary>
+    /// Represents a Trace Start/Stop where the Start and Stop are called automatically
+    /// </summary>;
+    public class TraceLogicalScope : IDisposable
+    {
+        /// <summary>
+        /// The source that we are tracing through as part of this scope
+        /// </summary>
+        private readonly TraceSource source;
+
+        /// <summary>
+        /// The name of the logical block that this scope represents
+        /// </summary>
+        private readonly string name;
+
+        /// <summary>
+        /// Constructor used to initialize the class
+        /// </summary>
+        /// <param name="source">The source that we are tracing through as part of this scope</param>
+        /// <param name="name">The name of the logical block that this scope represents</param>
+        public TraceLogicalScope(TraceSource source, string name)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            this.source = source;
+            this.name = name;
+
+            this.source.TraceEvent(TraceEventType.Start, 0, String.Format("Starting {0}", name));
+        }
+
+        /// <summary>
+        /// Called when the object is cleaned up, to close the scope
+        /// </summary>
+        public void Dispose()
+        {
+            this.source.TraceEvent(TraceEventType.Stop, 0, String.Format("Stopping {0}", this.name));
+        }
+    }
+}
---
+++ Health/Health.Common/Diagnostics/TraceTransferScope.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Health.Common.Diagnostics
+{
+    // <summary>
+    /// Represents a Trace Transfer scope within which tracing Transfers to and from another Activity
+    /// </summary>
+    public class TraceTransferScope : IDisposable
+    {
+        /// <summary>
+        /// The activity ID that we stitched away from when this scope was started
+        /// </summary>
+        private readonly Guid oldActivity;
+
+        /// <summary>
+        /// The activity ID that we stitched to when this scope was started
+        /// </summary>
+        private readonly Guid newActivity;
+
+        /// <summary>
+        /// The source that we are tracing through as part of this scope
+        /// </summary>
+        private readonly TraceSource source;
+
+        /// <summary>
+        /// The name of the activity that this scope represents
+        /// </summary>
+        private readonly string name;
+
+        /// <summary>
+        /// Constructor used to initialize the class
+        /// </summary>
+        /// <param name="source">The source that we are tracing through as part of this scope</param>
+        /// <param name="name">The name of the activity that this scope represents</param>
+        public TraceTransferScope(TraceSource source, string name)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            this.source = source;
+            this.oldActivity = Trace.CorrelationManager.ActivityId;
+            this.name = name;
+
+            this.newActivity = Guid.NewGuid();
+
+            if (this.oldActivity != Guid.Empty)
+            {
+                this.source.TraceTransfer(0, string.Format("Transferring to {0}...", this.name), this.newActivity);
+            }
+
+            Trace.CorrelationManager.ActivityId = this.newActivity;
+
+            this.source.TraceEvent(TraceEventType.Start, 0, this.name);
+        }
+
+        /// <summary>
+        /// Called when the object is cleaned up, to close the scope
+        /// </summary>
+        public void Dispose()
+        {
+            if (this.oldActivity != Guid.Empty)
+            {
+                this.source.TraceTransfer(0, string.Format("Transferring back from {0}...", this.name), this.oldActivity);
+            }
+
+            this.source.TraceEvent(TraceEventType.Stop, 0, this.name);
+
+            Trace.CorrelationManager.ActivityId = this.oldActivity;
+        }
+    }
+}
